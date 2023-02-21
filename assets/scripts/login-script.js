b6e72a88635f58952fce30fcc0238e0a44737f7c
Siska Andriani <125899333+Siska211@users.noreply.github.com>
2023-02-22 00:02:24 +0700

    Update login-script.js
    This is the script with use comment

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -25,11 +25,11 @@ loginFormElement.addEventListener('submit', function(event) {
 /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
     
-    /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
+   /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
     
-    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
+   /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
